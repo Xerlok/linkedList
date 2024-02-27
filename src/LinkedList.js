f5b57d5bb014dc14e5dfe6599e8e051238c3ac07
Xerlok <20791727+Xerlok@users.noreply.github.com>
2024-02-27 21:58:52 +0300

    Add more list functions

--- src/LinkedList.js
+++ src/LinkedList.js
@@ -1,4 +1,4 @@
-/* eslint-disable */
+/*eslint-disable*/
 import Node from './Node';
 
 export default class LinkedList {
@@ -9,21 +9,65 @@ export default class LinkedList {
 
   append(data) {
     const newNode = new Node(data);
-    if (this.head === null) {this.head = newNode;} 
-    else {
+    if (this.head === null) { this.head = newNode; } else {
       let currentNode = this.head;
-      while(currentNode.nextNode != null) {currentNode = currentNode.nextNode;}
+      while (currentNode.nextNode != null) { currentNode = currentNode.nextNode; }
       currentNode.nextNode = newNode;
     }
-    this.size++;
+    this.size += 1;
   }
 
   prepend(data) {
     const newNode = new Node(data);
-    if (this.head === null) {this.head = newNode}
-    else {
-      newNode.nextNode = this.head;
-      this.head = newNode;
+    newNode.nextNode = this.head;
+    this.head = newNode;
+    this.size += 1;
+  }
+
+  listSize() {
+    return this.size;
+  }
+
+  listHead() {
+    return this.head;
+  }
+
+  listTail() {
+    let currentNode = this.head;
+    while (currentNode.nextNode != null) { currentNode = currentNode.nextNode; }
+    return currentNode;
+  }
+
+  listAt(index) {
+    let currentNode = this.head;
+    if (index === 0) { return this.head; }
+    for (let i = 0; i < index; i += 1) {
+      currentNode = currentNode.nextNode;
+    }
+    return currentNode;
+  }
+
+  pop() {
+    if (this.head === null) { 
+      throw new Error('No items in the list!');
+    }
+    try {
+      if (this.size === 1) {
+        this.head = null;
+        this.size -= 1;
+      } else {
+        let currentNode = this.head;
+        let previousNode = null;
+        while (currentNode.nextNode != null) {
+          previousNode = currentNode;
+          currentNode = currentNode.nextNode;
+        }
+        currentNode = null;
+        previousNode.nextNode = null;
+        this.size -= 1;
+      }
+    } catch (e) {
+      console.error(e);
     }
   }
 }
